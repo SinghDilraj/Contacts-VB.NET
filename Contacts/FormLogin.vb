fc43b31421b0a7c3ca87eeeabe2f578cdee7bd86
Dilraj Singh <dilrajkehra@gmail.com>
2019-06-10 09:58:34 -0500

    created login and registration methods

--- Contacts/FormLogin.vb
+++ Contacts/FormLogin.vb
@@ -1,3 +1,52 @@
-﻿Public Class FormLogin
+﻿Imports System.Data.OleDb
 
+Public Class FormLogin
+    Dim connection As New OleDbConnection(My.Settings.ContactsConnectionString)
+
+    Private Sub ButtonLogin_Click(sender As Object, e As EventArgs) Handles ButtonLogin.Click
+        If Email.Text = Nothing Or Password.Text = Nothing Then
+            MsgBox("Please enter your username and password.", MsgBoxStyle.Exclamation)
+        Else
+            If connection.State = ConnectionState.Closed Then
+                connection.Open()
+            End If
+            Dim cmd As New OleDbCommand("select count(*) from Users where Email=? and Password=?", connection)
+            cmd.Parameters.AddWithValue("@1", OleDbType.VarChar).Value = Email.Text
+            cmd.Parameters.AddWithValue("@2", OleDbType.VarChar).Value = Password.Text
+
+            Dim count = Convert.ToInt32(cmd.ExecuteScalar())
+
+            If (count > 0) Then
+                MsgBox("Login Successfull.", MsgBoxStyle.Information)
+            Else
+                MsgBox("Invalid Email or Password.", MsgBoxStyle.Critical)
+            End If
+        End If
+    End Sub
+
+    Private Sub ButtonRegister_Click(sender As Object, e As EventArgs) Handles ButtonRegister.Click
+        If Email.Text = Nothing Or Password.Text = Nothing Then
+            MsgBox("Please enter your username and password.", MsgBoxStyle.Exclamation)
+        Else
+            If connection.State = ConnectionState.Closed Then
+                connection.Open()
+            End If
+            Dim cmd As New OleDbCommand("select count(*) from Users where Email=? and Password=?", connection)
+            cmd.Parameters.AddWithValue("@1", OleDbType.VarChar).Value = Email.Text
+            cmd.Parameters.AddWithValue("@2", OleDbType.VarChar).Value = Password.Text
+
+            Dim count = Convert.ToInt32(cmd.ExecuteScalar())
+
+            If (count > 0) Then
+                MsgBox("User Cannot be Registered.", MsgBoxStyle.Critical)
+            Else
+                Dim cmnd As New OleDbCommand("Insert into Users([Email], [Password]) Values (?, ?)", connection)
+                cmnd.Parameters.AddWithValue("@1", OleDbType.VarChar).Value = Email.Text
+                cmnd.Parameters.AddWithValue("@2", OleDbType.VarChar).Value = Password.Text
+                cmnd.ExecuteScalar()
+
+                MsgBox("Registration Successful.", MsgBoxStyle.Information)
+            End If
+        End If
+    End Sub
 End Class
